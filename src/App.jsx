3653d4a83e0a51b2269ed8bb50782c471686f13f
santosh <yousilenceplease@gmail.com>
2025-02-02 16:00:45 +0530

    Update Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './styles/main.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import Header from './components/Layouts/Header';
 function App() {
 
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route path='/about' element={<h1>About</h1>} />
+          <Route path='/menu' element={<h1>Our Menu</h1>} />
+          <Route path='/shop' element={<h1>Shop</h1>} />
+          <Route path='/blog' element={<h1>Blog</h1>} />
+          <Route path='/contact' element={<h1>Contact</h1>} />
         </Routes>
       </Router>
     </>
